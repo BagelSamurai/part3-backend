aef7ab9ee199551116e9962869c8c572e694da51
Darthwiings <105502133+Darthwiings@users.noreply.github.com>
2023-12-21 19:43:03 -0600

    Connected database and backend

--- index.js
+++ index.js
@@ -1,70 +1,74 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(express.static("dist"));
 
+const Entry = require("./models/Entry");
 app.use(express.json());
 app.use(cors());
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
 
-let data = [
-  {
-    name: "Arto Hellas",
-    number: "888",
-    id: 1,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "gg",
-    number: "66",
-    id: 4,
-  },
-  {
-    name: "gwegweg",
-    number: "15351",
-    id: 5,
-  },
-  {
-    name: "jj",
-    number: "55",
-    id: 6,
-  },
-  {
-    name: "rf32r32",
-    number: "r32",
-    id: 7,
-  },
-  {
-    name: "g",
-    number: "g",
-    id: 8,
-  },
-];
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
 
+  next(error);
+};
+app.use(errorHandler);
 app.get("/persons", (req, res) => {
-  res.json(data);
+  Entry.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
-app.post("/persons", (req, res) => {
-  const maxId = data.length > 0 ? Math.max(...data.map((n) => n.id)) : 0;
+app.get("/persons/:id", (req, res) => {
+  Entry.findById(req.params.id).then((entry) => {
+    res.json(entry);
+  });
+});
+app.post("/persons", (req, res, next) => {
   const body = req.body;
-  const newPerson = {
+
+  const newPerson = new Entry({
     name: body.name,
     number: body.number,
-    id: maxId + 1,
-  };
-  data = data.concat(newPerson);
-  res.json(newPerson);
+  });
+  newPerson
+    .save()
+    .then((savedEntry) => {
+      res.json(savedEntry);
+    })
+    .catch((error) => next(error));
 });
-app.delete("/persons/:id", (req, res) => {
-  const personId = Number(req.params.id);
-  data = data.filter((person) => person.id !== personId);
-  res.status(204).end();
+app.delete("/persons/:id", (req, res, next) => {
+  Entry.findByIdAndDelete(req.params.id)
+    .then((deletedEntry) => {
+      if (deletedEntry) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ error: "Entry not found" });
+      }
+    })
+    .catch((error) => next(error));
 });
+app.put("/persons/:id", (req, res, next) => {
+  const body = req.body;
 
-const PORT = process.env.PORT || 3001;
+  const entry = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Entry.findByIdAndUpdate(req.params.id, entry, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
